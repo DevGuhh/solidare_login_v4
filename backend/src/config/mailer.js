da3c30d28171efdd841b688bef9c4a1d07714e89
DevGuhh <gusta20042007@gmail.com>
2026-08-05 08:44:56 -0300

    Altera SMTP para porta 587

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -1,24 +1,31 @@
-import nodemailer from "nodemailer"
+import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  //service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 export async function sendMail(to, subject, html) {
-    try {
-        await transporter.sendMail({
-            from: `"Suporte" <${process.env.EMAIL_USER}>`,
-            to,
-            subject: "Redefinição de senha | Instituto Solidare",
-            html,
-        }),
-        console.log("E-mail enviado com sucesso!")
-    } catch (error) {
-        console.error("Erro ao enviar e-mail:", error)
-        throw error;
-    }
+  try {
+    await transporter.verify();
+    console.log("SMTP conectado!");
+
+    (await transporter.sendMail({
+      from: `"Suporte" <${process.env.EMAIL_USER}>`,
+      to,
+      subject: "Redefinição de senha | Instituto Solidare",
+      html,
+    }),
+      console.log("E-mail enviado com sucesso!"));
+  } catch (error) {
+    console.error("Erro ao enviar e-mail:", error);
+    throw error;
+  }
 }
