8a887f144482bfb285955adf0153df3c454a70b5
DevGuhh <gusta20042007@gmail.com>
2026-08-07 08:28:53 -0300

    feat: organizado o controller e routes do QrCode.

--- backend/src/routes/qrcodeRoutes.js
+++ backend/src/routes/qrcodeRoutes.js
@@ -1,60 +1,14 @@
 import express from "express";
-
-import {
-  listarQRCodes,
-  criarQRCode,
-  buscarQRCode,
-  gerarImagemQRCode,
-  validarQRCode,
-  desativarQRCode,
-} from "../controllers/qrcodeController.js";
-
+import qrcodeController from "../controllers/qrcodeController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// =====================================================
-// LISTAR QR CODES
-// GET /qrcodes
-// =====================================================
-
-router.get("/", protect, listarQRCodes);
-
-// =====================================================
-// CRIAR QR CODE
-// POST /qrcodes
-// =====================================================
-
-router.post("/", protect, criarQRCode);
-
-
-
-// =====================================================
-// VALIDAR QR CODE ESCANEADO
-// GET /qrcodes/:codigo/validar
-// =====================================================
-
-router.get("/:codigo/validar", protect, validarQRCode);
-
-// =====================================================
-// GERAR IMAGEM PNG DO QR CODE
-// GET /qrcodes/:codigo/imagem
-// =====================================================
-
-router.get("/:codigo/imagem", protect, gerarImagemQRCode);
-
-// =====================================================
-// BUSCAR QR CODE
-// GET /qrcodes/:codigo
-// =====================================================
-
-router.get("/:codigo", protect, buscarQRCode);
-
-// =====================================================
-// DESATIVAR QR CODE
-// PATCH /qrcodes/:id
-// =====================================================
-
-router.patch("/:id", protect, desativarQRCode);
+router.get("/", protect, qrcodeController.listarQRCodes);
+router.post("/", protect, qrcodeController.criarQRCode);
+router.get("/:codigo/validar", protect, qrcodeController.validarQRCode);
+router.get("/:codigo/imagem", protect, qrcodeController.gerarImagemQRCode);
+router.get("/:codigo", protect, qrcodeController.buscarQRCode);
+router.patch("/:id", protect, qrcodeController.desativarQRCode);
 
 export default router;
